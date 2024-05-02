e71b33c32339dc91ccaad1ce2c5c408ca010bd11
Shiv <sky.shivy@gmail.com>
2024-05-02 13:09:00 +0530

    new screen

--- lib/files/reusable_widgets/buttons/buy_button.dart
+++ lib/files/reusable_widgets/buttons/buy_button.dart
@@ -9,41 +9,6 @@ import 'package:you_yemen/files/reusable_widgets/u_text.dart';
 import 'package:you_yemen/files/translation/strings.dart';
 import 'package:you_yemen/files/utility/colors.dart';
 
-// Widget buyButton(
-//   Function()? onTap ) {
-//   return InkWell(
-//     onTap: onTap,
-//      //!=null ?()=> showBuyPopup(context):null,
-
-//     child: Container(
-//         height: 40,
-//         decoration: BoxDecoration(
-//           color: yellow,
-//           borderRadius: BorderRadius.circular(20),
-//           border: Border.all(color: transparent),
-//         ),
-//         child: const Padding(
-//           padding: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-//           child: Row(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             crossAxisAlignment: CrossAxisAlignment.center,
-//             mainAxisSize: MainAxisSize.min,
-//             children: [
-//               Icon(
-//                 Icons.card_travel,
-//                 size: 20,
-//               ),
-//               SizedBox(width: 4),
-//               UText(
-//                 title: buyStr,
-//                 enfontName: FontName.helveticaBold,
-//               ),
-//             ],
-//           ),
-//         )),
-//   );
-// }
-
 Widget buyButton(TuneInfo info, Function()? onTap) {
   return ResponsiveBuilder(
     builder: (context, sizingInformation) {
@@ -56,7 +21,7 @@ Widget buyButton(TuneInfo info, Function()? onTap) {
         child: Container(
             height: 40,
             decoration: BoxDecoration(
-              color: yellow,
+              color: black,
               borderRadius: BorderRadius.circular(20),
               border: Border.all(color: transparent),
             ),
@@ -70,9 +35,11 @@ Widget buyButton(TuneInfo info, Function()? onTap) {
                   Icon(
                     Icons.card_travel,
                     size: 16,
+                    color: white,
                   ),
                   SizedBox(width: 4),
                   UText(
+                    textColor: white,
                     title: buyStr,
                     enfontName: FontName.helveticaBold,
                   ),
