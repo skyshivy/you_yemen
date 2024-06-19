ba65da4e20fc16cec23aba713dbb56c4d8e31d16
Shiv <sky.shivy@gmail.com>
2024-06-19 10:28:02 +0530

    new change

--- lib/files/api_self_care/get_banner_list_api.dart
+++ lib/files/api_self_care/get_banner_list_api.dart
@@ -2,12 +2,12 @@ import 'package:you_yemen/files/network_manager/network_manager.dart';
 import 'package:you_yemen/files/store_manager/store_manager.dart';
 import 'package:you_yemen/files/utility/constants.dart';
 
-
 getBannerlistApi() async {
-  String url = "http://10.0.14.4:5892/selfcare/get-banner-list?channelId=$channelId&languageCode=${StoreManager().languageCode}";
- Map<String, dynamic> header = {
-               "transId":"87863646846",
-             };
-  Map<String ,dynamic> response=await NetworkManager().get(url,  params: header);
+  String url =
+      "http://10.0.14.4:5892/selfcare/get-banner-list?channelId=$channelId&languageCode=${StoreManager().languageCode}";
+  Map<String, String> header = {
+    "transId": "87863646846",
+  };
+  Map<String, dynamic> response =
+      await NetworkManager().get(url, params: header);
 }
-
